ea89f80f69dd74e6c5e8b99d8f16262d7ee4b460
Messorah <gabrielcambara123@gmail.com>
2023-12-30 17:29:25 -0300

    esquiva automatica

--- joguinsimples.py
+++ joguinsimples.py
@@ -6,7 +6,7 @@ def main():
     print('Você tem 10 de vida e causa 3 de dano contra ele com 25 de vida e causa 4 de dano')
     print('Você tem 3 poções de cura que curam 4 pontos de vida')
     print('Você e o boss têm 10% de chance de dar e receber críticos, causando o dobro de dano')
-    print('Você tem 75% de chance de se esquivar, mas você da 3 de dano e se você não se esquivar você toma 3 de dano\033[m')
+    print('Você tem 75% de chance de se esquivar, mas você da 2 de dano e se você não se esquivar você toma 3 de dano\033[m')
 def vida():
     vida_jogador = 10
     vida_mostro = 25
@@ -30,7 +30,7 @@ def vida():
                         if lado == 'direita':
                             if chance_esquiva == 1 or chance_esquiva == 2:
                                 vida_mostro -= 2
-                                print(f'\033[1;34mVocê se esquivou para a direita e deu 3 de dano Sua vida {vida_jogador} \ Vida do monstro: {vida_mostro}\033[m')
+                                print(f'\033[1;34mVocê se esquivou para a direita e deu 2 de dano Sua vida {vida_jogador} \ Vida do monstro: {vida_mostro}\033[m')
                             else:
                                 vida_jogador -= 3
                                 print(f'\033[1;31mvocê não conseguiu se esquivar e tomou 3 de dano Sua vida {vida_jogador} \ Vida do monstro: {vida_mostro}\033[m')
@@ -38,7 +38,7 @@ def vida():
                         elif lado == 'esquerda':
                             if chance_esquiva == 1 or chance_esquiva == 2:
                                 vida_mostro -= 2
-                                print(f'\033[1;34mVocê se esquivou para a esquerda e deu 3 de dano Sua vida {vida_jogador} \ Vida do monstro: {vida_mostro}\033[m')
+                                print(f'\033[1;34mVocê se esquivou para a esquerda e deu 2 de dano Sua vida {vida_jogador} \ Vida do monstro: {vida_mostro}\033[m')
                             
                             else:
                                 vida_jogador -= 3
@@ -48,7 +48,7 @@ def vida():
                             print('erro')
                 tot += 1
                 if tot > 1 and vida_jogador < 10:
-                    if cura > 0 and vida_jogador <=6:
+                    if cura > 0 and vida_jogador <= 6:
                         while True:
                             poçao = str(input(f'\033[1;32mVocê tem {cura} poções de cura. Precione 1 para tomar uma poção de cura e 2 para não tomar:\033[m '))
                             if poçao == '1':
@@ -65,15 +65,21 @@ def vida():
                     print()
                 else:   
                     #usar aqui
+                    chance_desvio_automatico = randint(1, 5)
                     chance_acerto_monstro = randint(0,1)
                     chance_acerto_jogador = randint(0,1)
                     critico_pessoa = randint(1, 10)
+                    if chance_desvio_automatico == 1:
+                        print(f'você se desviou Sua vida {vida_jogador} \ Vida do monstro: {vida_mostro}')
+                        ataque = str(input('Aperte p para atacar ou e para esquivar: ')).lower()
+                    elif chance_desvio_automatico == 2:
+                        print(f'o monstro se desviou Sua vida {vida_jogador} \ Vida do monstro: {vida_mostro}')
+                        ataque = str(input('Aperte p para atacar ou e para esquivar: ')).lower()
                     if critico_pessoa == 1:
                         vida_mostro -= 6    
                         print(f'\033[1;35mVocê deu um CRÍTICO de 6 de dano\033[m.')
                         print(f'Sua vida {vida_jogador} \ Vida do monstro: {vida_mostro}') 
                     else:
-                        #o bug que nao volta pra selecionar se bate ou esquiva
                         if chance_acerto_monstro == 0:  
                             if super.count(1) < 2:  
                                 vida_mostro -= 3
@@ -86,6 +92,7 @@ def vida():
                             if super.count(1) == 2:
                                 vida_mostro -= 5
                                 print(f'voce usou o super Sua vida {vida_jogador} \ Vida do monstro: {vida_mostro}')
+                                ataque = str(input('Aperte p para atacar ou e para esquivar: ')).lower()
                                 while carregado != 0:
                                     super.remove(1)
                                     carregado -= 1                       
